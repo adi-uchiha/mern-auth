0d9a0f7bc6defc65ee3faa40cac74a6d0b200dec
Aditya Shelke <adi.cs.night@gmail.com>
2022-09-01 20:34:04 +0530

    User Controller and UserModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
+
 
 const userSchema = new mongoose.Schema({
     email : {
@@ -17,16 +19,41 @@ const userSchema = new mongoose.Schema({
 userSchema.statics.signup = async function( email, password ){
     //create a user with that email and password
 
+    if(!email || !password){
+        throw Error("All fields must be filled")
+    }
+    if (!validator.isEmail(email)){
+        throw Error('Enter a valid email')
+    }
+    if (!validator.isStrongPassword(password)){
+        throw Error('Password not strong enough')
+    }
+
     const exist = await this.findOne({ email })
 
     if(exist){
-        throw Error('Email Already in use')
+        throw Error('Email Already registered')
     }
     //hashing the password
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash( password, salt )
 
     const user = await this.create({  email, password:hash  })
+    return user
+}
+
+//Static method for login
+userSchema.statics.login = async function(email, password){
+    if(!email || !password){
+        throw Error('Provide email and password')
+    }
+    const user = await this.findOne({email})
+    if(!user){
+        throw Error('Email not registered')
+    }
+    const match = await bcrypt.compare(password, user.password)
+    if(!match){throw Error('Incorrect password')}
+
     return user
 }
 
