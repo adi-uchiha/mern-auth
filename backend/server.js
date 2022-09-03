750c4c2d694fb8ec7ff4b0c121bf54e5d42eeccc
Aditya Shelke <adi.cs.night@gmail.com>
2022-09-03 18:50:26 +0530

    homepage url fix

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({path: __dirname+'/.env'});
+}
+
 
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path');
+
+
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
+
 
 // express app
 const app = express()
@@ -17,18 +24,30 @@ app.use((req, res, next) => {
 })
 
 // reqistering routes
-app.use('/api/workouts', workoutRoutes)
-app.use('/api/user', userRoutes)
+// app.use('/api/workouts', workoutRoutes)
+// app.use('/api/user', userRoutes)
+
+app.use('/api/workouts', require(path.join(__dirname, 'api', 'routes', 'workouts.js')));
+app.use('/api/user', require(path.join(__dirname, 'api', 'routes', 'user.js')));
+
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('connected to database')
     // listen to port
-    app.listen(process.env.PORT, () => {
-      console.log('listening for requests on port', process.env.PORT)
+    app.listen(process.env.PORT || 4000, () => {
+      console.log('listening for requests on port')
     })
   })
   .catch((err) => {
     console.log(err)
   }) 
+
+// static files (build of your frontend)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
+  })
+}
